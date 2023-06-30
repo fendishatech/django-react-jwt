6e25934d4a2b6f383a3586d2d9e8c9a05b0684f6
fendishatech <liyukibir@gmail.com>
2023-06-30 09:07:08 -0700

    setup routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./index.css";
+
 import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoginPage />
+    <Router>
+      <Routes>
+        <Route element={<LoginPage />} path="/" exact />
+        <Route element={<HomePage />} path="/home" />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
   return (
     <div>
-      <p className="text-green-400">You Are Logged In</p>
+      <p className="pt-6 text-2xl text-center font-bold text-green-400">
+        Hello User You Are Logged In !
+      </p>
     </div>
   );
 };
